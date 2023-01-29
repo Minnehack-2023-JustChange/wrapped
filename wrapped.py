715bdbd48fd5c3dffdddf4f862e64123b3d3b9b4
benjaminlindeen <benjaminlindeen@outlook.com>
2023-01-29 11:18:57 -0600

    wrapped-done

--- wrapped.py
+++ wrapped.py
@@ -8,20 +8,20 @@ def wrapped_text(dollar_total, type_donation, charity, month):
     fnt_3 = ImageFont.truetype('Arial.ttf', 30)
     fnt_4 = ImageFont.truetype('Arial.ttf', 25)
 
-    draw.text((20, 10), "2023 JustChange Wrapped", font = fnt_2, fill = (0, 111, 100))
+    draw.text((20, 10), "2023 JustChange Wrapped", font = fnt_2, fill = (255, 255, 255))
 
-    draw.text((20, 375), "Dollars Donated", font = fnt_3, fill = (0, 111, 100))
-    draw.text((20, 425), "$" + str(dollar_total), font = fnt, fill = (0, 111, 100))
+    draw.text((20, 375), "Dollars Donated", font = fnt_3, fill = (255, 255, 255))
+    draw.text((20, 425), "$" + str(dollar_total), font = fnt, fill = (255, 255, 255))
 
-    draw.text((20, 500), "Donation Type", font = fnt_3, fill = (0, 111, 100))
-    draw.text((20, 550), str(type_donation), font=fnt_3, fill=(0, 111, 100))
+    draw.text((20, 500), "Donation Type", font = fnt_3, fill = (255, 255, 255))
+    draw.text((20, 550), str(type_donation), font = fnt_3, fill = (255, 255, 255))
 
-    draw.text((275, 75), "Top Charities", font = fnt_3, fill = (0, 111, 100))
+    draw.text((275, 75), "Top Charities", font = fnt_3, fill = (255, 255, 255))
     for i in range(5):
-        draw.text((275, 125 + i * 40), str(charity[i]), font = fnt_4, fill = (0, 111, 100))
+        draw.text((275, 125 + i * 40), str(charity[i]), font = fnt_4, fill = (255, 255, 255))
 
-    draw.text((275, 375), "Top Months", font = fnt_3, fill = (0, 111, 100))
+    draw.text((275, 375), "Top Months", font = fnt_3, fill = (255, 255, 255))
     for k in range(5):
-        draw.text((275, 425 + k * 40), str(month[k]), font = fnt_4, fill = (0, 111, 100))
+        draw.text((275, 425 + k * 40), str(month[k]), font = fnt_4, fill = (255, 255, 255))
 
     img.save('wrapped_finished.png')
